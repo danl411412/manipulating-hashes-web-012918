41f2040919b1dcade15ce64a97323c4cc1f4295c
danl411412 <daan.b.lee@gmail.com>
2018-01-25 06:15:55 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,17 @@ def first_challenge
   }
 
   #your code here
-
+  contacts.each do |key, value|
+    if key == "Freddie Mercury"
+      key.each do |key, value|
+        if key == :favorite_icecream_flavors
+          key.delete_if do |x|
+            x == "strawberry"
+          end
+        end
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
